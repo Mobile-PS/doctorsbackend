85370d5458ec7fcf28e43182b5b7bac147b1015a
abhilash-26 <krabhilash226@gmail.com>
2022-05-18 18:20:56 +0530

    initiual setup

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,23 @@
-FROM node:8-alpine
+# FROM node:8-alpine
 
-EXPOSE 3000
+# EXPOSE 3000
+
+# ARG NODE_ENV
+# ENV NODE_ENV $NODE_ENV
+
+# RUN mkdir /app
+# WORKDIR /app
+# ADD package.json yarn.lock /app/
+# RUN yarn --pure-lockfile
+# ADD . /app
+
+# CMD ["yarn", "docker:start"]
+FROM node:latest
+
+EXPOSE 8080
 
 ARG NODE_ENV
-ENV NODE_ENV $NODE_ENV
+ENV NODE_ENV $NODE_ENV 
 
 RUN mkdir /app
 WORKDIR /app
--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -1,104 +1,73 @@
-const httpStatus = require('http-status');
-const { omit } = require('lodash');
-const User = require('../models/user.model');
+const httpStatus = require("http-status");
+const { omit } = require("lodash");
+const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { sucessResponse, failResponse } = require("../utils/responceHandler");
+const { jwtSecret } = require("../../config/vars");
 
-/**
- * Load user and append to req.
- * @public
- */
-exports.load = async (req, res, next, id) => {
+exports.userSignUp = async (req, res) => {
   try {
-    const user = await User.get(id);
-    req.locals = { user };
-    return next();
+    const { name, email, password, mobile, role } = req.body;
+    const savedemail = await User.findOne({
+      email: email,
+    });
+    if (savedemail) {
+      return res.send({ status: 1, message: "email is already in use" });
+    }
+
+    //hash the password using bcryptjs library
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const createdUser = await User.create({
+      name: name,
+      email: email,
+      mobile: mobile,
+      password: hashPassword,
+      role: role,
+    });
+    const userTo = {
+      useremail: email,
+      role: createdUser.role,
+    };
+    const accesToken = jwt.sign(userTo, jwtSecret);
+    const dataToSend = {
+      email: createdUser.email,
+      name: name,
+      token: accesToken,
+    };
+    sucessResponse(res, dataToSend, 200, "User registered successfully");
   } catch (error) {
-    return next(error);
+    console.log(error.message);
   }
 };
 
-/**
- * Get user
- * @public
- */
-exports.get = (req, res) => res.json(req.locals.user.transform());
-
-/**
- * Get logged in user info
- * @public
- */
-exports.loggedIn = (req, res) => res.json(req.user.transform());
-
-/**
- * Create new user
- * @public
- */
-exports.create = async (req, res, next) => {
+exports.userLogin = async (req, res) => {
   try {
-    const user = new User(req.body);
-    const savedUser = await user.save();
-    res.status(httpStatus.CREATED);
-    res.json(savedUser.transform());
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.send({ message: "email is incorrect" });
+
+    const validPass = await bcrypt.compare(password, user.password);
+    if (!validPass) return res.send({ message: "invalid password" });
+
+    const userTo = {
+      useremail: email,
+      role: createdUser.role,
+    };
+
+    // creating the jwt token
+    const accessToken = jwt.sign(userTo, jwtSecret);
+    const dataToSend = {
+      email: user.email,
+      name: user.name,
+      token: accessToken,
+    };
+    sucessResponse(res, dataToSend, 200, "User Logged in successfully");
   } catch (error) {
-    next(User.checkDuplicateEmail(error));
+    // errorResponse(res, error);
+    res.send(error);
   }
-};
-
-/**
- * Replace existing user
- * @public
- */
-exports.replace = async (req, res, next) => {
-  try {
-    const { user } = req.locals;
-    const newUser = new User(req.body);
-    const ommitRole = user.role !== 'admin' ? 'role' : '';
-    const newUserObject = omit(newUser.toObject(), '_id', ommitRole);
-
-    await user.updateOne(newUserObject, { override: true, upsert: true });
-    const savedUser = await User.findById(user._id);
-
-    res.json(savedUser.transform());
-  } catch (error) {
-    next(User.checkDuplicateEmail(error));
-  }
-};
-
-/**
- * Update existing user
- * @public
- */
-exports.update = (req, res, next) => {
-  const ommitRole = req.locals.user.role !== 'admin' ? 'role' : '';
-  const updatedUser = omit(req.body, ommitRole);
-  const user = Object.assign(req.locals.user, updatedUser);
-
-  user.save()
-    .then((savedUser) => res.json(savedUser.transform()))
-    .catch((e) => next(User.checkDuplicateEmail(e)));
-};
-
-/**
- * Get user list
- * @public
- */
-exports.list = async (req, res, next) => {
-  try {
-    const users = await User.list(req.query);
-    const transformedUsers = users.map((user) => user.transform());
-    res.json(transformedUsers);
-  } catch (error) {
-    next(error);
-  }
-};
-
-/**
- * Delete user
- * @public
- */
-exports.remove = (req, res, next) => {
-  const { user } = req.locals;
-
-  user.remove()
-    .then(() => res.status(httpStatus.NO_CONTENT).end())
-    .catch((e) => next(e));
 };
