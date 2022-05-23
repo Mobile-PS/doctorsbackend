47fbfc1b92b78852968ec00bd6402172543e488a
abhilash-26 <krabhilash226@gmail.com>
2022-05-24 00:38:26 +0530

    minor fix

--- src/api/services/emails/emailProvider.js
+++ src/api/services/emails/emailProvider.js
@@ -1,6 +1,7 @@
 const Email = require("email-templates");
 const nodemailer = require("nodemailer");
 const { emailConfig } = require("../../../config/vars");
+const logger = require("../../../config/logger");
 
 exports.sendEmail = async (email, otp) => {
   try {
@@ -27,8 +28,10 @@ exports.sendEmail = async (email, otp) => {
     };
     await transporter.sendMail(mailOptions, function (error, info) {
       if (error) {
+        logger.info(error);
         return error;
       } else {
+        logger.info(info.response);
         return info.response;
       }
     });
